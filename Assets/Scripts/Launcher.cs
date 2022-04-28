5101275be3ec1833f9e1071ad74058441c6d7fe2
Tanner Parker <tpaaka73@gmail.com>
2022-04-28 17:15:57 -0500

    more updates

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class Launcher : MonoBehaviourPunCallbacks
 {
     [SerializeField] TMP_InputField roomnameIF;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomText;
 
     // Start is called before the first frame update
     void Start()
@@ -38,15 +40,27 @@ public class Launcher : MonoBehaviourPunCallbacks
     }
     public override void OnJoinedRoom()
     {
-
+        roomText.text = PhotonNetwork.CurrentRoom.Name;
+        MenuManager.Instance.OpenMenu("room");
         Debug.Log("Joined Room");
     }
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
-        base.OnCreateRoomFailed(returnCode, message);
+        errorText.text = "Room Creation Failed: " + message; 
+        MenuManager.Instance.OpenMenu("error");
         Debug.Log("Room Failed to Create");
     }
-
+    public void LeaveRoom()
+    {
+        Debug.Log("Left Room");
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("loading");
+    }
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("title");
+    }
+    
     // Update is called once per frame
     void Update()
     {
